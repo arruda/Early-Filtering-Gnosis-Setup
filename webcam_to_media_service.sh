e48e36ba153b5ac21725371d866b6134afba57e2
Felipe Arruda Pontes <felipe.arruda.pontes@insight-centre.org>
2019-09-03 15:56:30 +0100

    Adding font and timestamp text to the webcamstream

--- webcam_to_media_service.sh
+++ webcam_to_media_service.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./ffmpeg_bin/ffmpeg-linux64-v3.3.1 -f video4linux2 -i /dev/video0 -c:v libx264 -an -f flv rtmp://localhost/live/mystream
+
+./ffmpeg_bin/ffmpeg-linux64-v3.3.1 -f video4linux2 -i /dev/video0 -c:v libx264 -vf "drawtext=ffmpeg_bin/KhmerOS.ttf:fontcolor=white:text='%{pts\:gmtime\:1507046400\:%d-%m-%Y %T}:fontsize=14'[out]" -an -f flv rtmp://localhost/live/mystream
