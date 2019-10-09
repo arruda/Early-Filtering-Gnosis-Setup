4dbe6ae173448dc3573faac80e45fd0a4330bc2e
Felipe Arruda Pontes <felipe.arruda.pontes@insight-centre.org>
2019-10-09 13:59:55 +0100

    fixing last cmt

--- compose-all.sh
+++ compose-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# docker-compose -f docker-compose.yml -f docker-compose-with-gpu-obj-detection.yml -f docker-compose-with-media-server.yml "$@"
-docker-compose -f docker-compose.yml -f docker-compose-with-media-server.yml "$@"
+docker-compose -f docker-compose.yml -f docker-compose-with-gpu-obj-detection.yml -f docker-compose-with-media-server.yml "$@"
+# docker-compose -f docker-compose.yml -f docker-compose-with-media-server.yml "$@"
 
