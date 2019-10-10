b297160a9fbde5e8ab2434546ba1a95aeb5909f7
Felipe Arruda Pontes <felipe.arruda.pontes@insight-centre.org>
2019-10-10 17:13:48 +0100

    adding simpler media-server

--- compose-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose.yml -f docker-compose-with-gpu-obj-detection.yml -f docker-compose-with-media-server.yml "$@"
-# docker-compose -f docker-compose.yml -f docker-compose-with-media-server.yml "$@"
-
