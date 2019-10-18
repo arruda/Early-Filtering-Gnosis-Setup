355314d8da4a239b70a6727edb7ef043c9fc43b7
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2019-10-18 13:14:44 +0100

    improved override

--- compose-media.sh
+++ compose-media.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+if [[ ! -e docker-compose.override.yml ]]; then
+    echo "version: '2.3'" > docker-compose.override.yml
+fi
 docker-compose -f docker-compose.yml -f docker-compose-with-media-server.yml -f docker-compose.override.yml "$@"
