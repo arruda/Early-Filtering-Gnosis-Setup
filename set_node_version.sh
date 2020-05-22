eed596ca319c4e29069d3f35212b91c410d9b106
Felipe Arruda Pontes <felipe.arruda.pontes@insight-centre.org>
2020-05-22 19:29:04 +0100

    making the set node version change the example.env as well, just to make things easier

--- set_node_version.sh
+++ set_node_version.sh
@@ -5,4 +5,5 @@ echo "Changing this node version to ${VERSION}"
 git fetch origin
 git checkout ${VERSION}
 sed -i 's,IMAGE_TAG=.*,IMAGE_TAG='"${VERSION}"',' .env
+sed -i 's,IMAGE_TAG=.*,IMAGE_TAG='"${VERSION}"',' example.env
 
