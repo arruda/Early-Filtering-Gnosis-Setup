ecf4dab3714283cbee0f8a57823db254a057de44
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2021-06-24 17:45:17 +0100

    fixingf bumpversion script

--- bumpversion.sh
+++ bumpversion.sh
@@ -4,12 +4,12 @@ set -e
 VERSION_BUMP_TYPE=$1
 
 echo "Bumping version..."
-bump2version ${VERSION_BUMP_TYPE}
+# bump2version ${VERSION_BUMP_TYPE}
 
 
 echo "Starting tagging process..."
 
-DOCKER_REGISTRY="registry.insight-centre.org/sit/mps/felipe-phd"
+DOCKER_REGISTRY="registry.insight-centre.org/sit/mps/felipe-phd/"
 
 SERVICE_PROJECT_LIST="window-manager service-registry adaptation-knowledge event-dispatcher matcher scheduler object-detection-service object-detection-service-gpu adaptation-planner preprocessing-service adaptation-analyser adaptation-monitor client-manager forwarder"
 
@@ -20,26 +20,26 @@ echo 'Tagging containers and git repositories...'
 echo 'Ensure latest version of all images are available...'
 for SERVICE_NAME in $SERVICE_PROJECT_LIST; do
     IMAGE=${DOCKER_REGISTRY}${SERVICE_NAME}
+    IMAGE_AND_TAG=${IMAGE}:master
+    NEW_TAG=${IMAGE}':'${NEW_VERSION}
     if [[ $SERVICE_NAME == "object-detection-service-gpu" ]]
     then
         SERVICE_NAME="object-detection-service"
+        IMAGE=${DOCKER_REGISTRY}${SERVICE_NAME}
         IMAGE_AND_TAG=${IMAGE}:master-gpu
+        NEW_TAG=${IMAGE}':'${NEW_VERSION}'-gpu'
     else
-        IMAGE_AND_TAG=${IMAGE}:master
+        echo "Creating GIT tag in gitlab for ${SERVICE_NAME} ${NEW_VERSION}"
+        python tag_repositories.py ${SIT_TOKEN} ${SERVICE_NAME} ${NEW_VERSION}
     fi
 
-    echo 'Re-pulling latest image: ${LATEST_IMAGE}'
-    docker pull ${LATEST_IMAGE}
-    NEW_TAG=${IMAGE}':'${NEW_VERSION}
+    echo "Re-pulling latest image: ${IMAGE_AND_TAG}"
+    docker pull ${IMAGE_AND_TAG}
 
-    echo "Tagging Docker image: " $LATEST_IMAGE " > " $NEW_TAG
-    # docker tag $LATEST_IMAGE $NEW_TAG
+    echo "Tagging Docker image: " $IMAGE_AND_TAG " > " $NEW_TAG
+    docker tag $IMAGE_AND_TAG $NEW_TAG
     echo "pushing new Docker Image Tag to repository"
-    # docker push $NEW_TAG
-
-    echo "Creating GIT tag in gitlab for ${SERVICE_NAME} ${NEW_VERSION}"
-    # python tag_repositories.py ${SIT_TOKEN} ${SERVICE_NAME} ${NEW_VERSION}
-
+    docker push $NEW_TAG
     echo " "
 done
 
