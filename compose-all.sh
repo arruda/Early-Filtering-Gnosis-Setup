c98046bdb4f8316adcb132bbd927179825c5f145
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2024-10-03 14:05:43 +0200

    setting up basic stuff to make it easier to run th exp

--- compose-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [[ ! -e docker-compose.override.yml ]]; then
-    echo "version: '2.3'" > docker-compose.override.yml
-fi
-docker-compose -f docker-compose.yml -f docker-compose-with-media-server.yml -f docker-compose-with-gpu-obj-detection.yml -f docker-compose.override.yml "$@"
